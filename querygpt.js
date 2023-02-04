d6d9e33333eb54755109f350e5225073270ac666
Your Name <you@example.com>
2023-02-04 19:42:09 +0200

    Implment Basics

--- querygpt.js
+++ querygpt.js
@@ -4,7 +4,7 @@ safe_pages_subject = ["science", "regular"]
 function send_data_to_analsis(page_url, data, chatgpt_answer){
 }
 params = safe_pages_subject.join(",")
-chat_query = "please classify the folowing context if the context is not good for kinds uner 12 then the first world of your answer should be unsafe with no commad other wise it should be one of the folowing words" + parmas 
+chat_query = "please classify the folowing context if the context is not good for kinds uner 12 then the first world of your answer should be unsafe with no comma or dot in end other wise it should be one of the folowing words" + parmas +"without comma or dot"
 
     
       const query = chat_query + document_text
